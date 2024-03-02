5df88a9628f46cf34618bf70f36205b0dfa24a7d
vedhamshbode <vedhamshbode@gmail.com>
2024-03-02 22:51:15 +0530

    diff_drive

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='bbot_sim' #<--- CHANGE ME
+    package_name='bbot_description' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
